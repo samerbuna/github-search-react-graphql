6f5740df33345237af00563de18a6016b6851721
Samer Buna <samer.buna@gmail.com>
2019-05-14 21:16:27 -0700

    Updates

---
+++ precache-manifest.3158f1bb3b8de8fbae1c82994b068e98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957f44c0c23c11839befd56a7385f940",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04cce73619a337b55fea",
+    "url": "/static/css/main.5cbdab00.chunk.css"
+  },
+  {
+    "revision": "1afa2f76323dc9c8d933",
+    "url": "/static/js/2.2bd5a440.chunk.js"
+  },
+  {
+    "revision": "04cce73619a337b55fea",
+    "url": "/static/js/main.7fa912bd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
